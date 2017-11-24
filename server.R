991c777454440ebfb37d9c81c74a01217815bffc
Aaron Nguyen <aaronluannguyen@gmail.com>
2017-11-23 20:13:09 -0800

    added rsconnect code to run in console. closes #7 which links up shiny with shiny token

--- server.R
+++ server.R
@@ -1,5 +1,12 @@
 # Server side file
 
+
+# rsconnect code to run in console below
+#rsconnect::setAccountInfo(name='aaron-nguyen',
+#                          token='F52E4104BEF9260EFB0008A71F2D69BE',
+#                          secret='EVjqAYj2wWuVoatja/OKJy4BytZGu6U974bMre0/')
+
+
 # Load necessary libraries 
 library(shiny)
 library(dplyr)
