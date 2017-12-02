1f9f5ffe525f3edc46b761cbb843947c31471e15
Chianson Siu <chanmancu@gmail.com>
2017-12-01 21:39:38 -0800

    Finish data cleansing function

--- scripts/time_filter/time_filter_script.R
+++ scripts/time_filter/time_filter_script.R
@@ -6,15 +6,18 @@ library(stringr)
 
 # Read in king county police call data and only keeping city and hour_of_day column
 police.data <- select(read.csv("data/king_county_data.csv", stringsAsFactors = FALSE), city, hour_of_day)
-# Cleaning data, removing empty strings, "N/A" and rows with numbers in the cities column instead of city names
-police.data <- police.data[which(police.data$city != "" & police.data$city != "N/A"), ]
+
+# Cleaning data by removing empty strings, "N/A", rows with numbers in the cities column instead of city names
+# and updating misspelled cities such as "Seattel" with the correct spelling, "Seattle"
 police.data <- police.data[which(grepl("^[0-9]+$", police.data$city) == FALSE), ]
 police.data$city <- str_to_title(police.data$city)
-fix.city.name <- list("Seatle" = "Seattle", 
-                      "Seatttle" = "Seattle", 
+correct.city.names <- list("Seatle" = "Seattle",
+                      "Seatttle" = "Seattle",
                       "Seattq" = "Seattle",
                       "Seatlle" = "Seattle",
                       "Sattle" = "Seattle",
+                      "Seattel" = "Seattle",
+                      "Seatte" = "Seattle",
                       "Aburn" = "Auburn",
                       "Aubirn" = "Auburn",
                       "New Castle" = "Newcastle",
@@ -22,15 +25,46 @@ fix.city.name <- list("Seatle" = "Seattle",
                       "Maple Valle" = "Maple Valley",
                       "Kenmre" = "Kenmore",
                       "Kenmroe" = "Kenmore",
+                      "Kenomre" = "Kenmore",
                       "Auburn Ave S" = "Auburn",
                       "Bujrien" = "Burien",
                       "Burieb" = "Burien",
-                      "Seatec" = "Seatac",
+                      "Seatec" = "SeaTac",
+                      "Seatac" = "SeaTac",
                       "Carnation, Wa 98014" = "Carnation", 
-                      "Woodinvile" = "Woodinville")
+                      "Woodinvile" = "Woodinville",
+                      "Tuckwila" = "Tukwila",
+                      "C17051435" = "",
+                      "Shorelinee" = "Shoreline",
+                      "Shorline" = "Shoreline",
+                      "Barring" = "Baring",
+                      "Safeway" = "",
+                      "White Cneter" = "White Center",
+                      "Snoqualmie Pass, Wa 98068" = "Snoqualmie",
+                      "Snoqualmie Pass" = "Snoqualmie",
+                      "Snoqulamie" = "Snoqualmie",
+                      "Desmoines" = "Des Moines",
+                      "Fred Meyer" = "",
+                      "Mount Lake Terrance" = "Mountlake Terrace",
+                      "Mount Lake Terrace" = "Mountlake Terrace",
+                      "Lynnwod" = "Lynnwood",
+                      "Vashon" = "Vashon Island"
+                      )
+
+CleanData <- function(vec){
+  check.name <- match(vec, names(correct.city.names))
+  if(!is.na(check.name)){
+    vec <- correct.city.names[check.name] 
+  }
+  return(vec)
+}
+police.data$city <- unlist(sapply(police.data$city, CleanData), use.names = FALSE)
+
+# Remove rows with empty strings or "N/A"
+police.data <- police.data[which(police.data$city != "" & police.data$city != "N/A"), ]
 
 # Get unique city names
-cities <- c(str_to_title(unique(police.data$city)), "All")
+cities <- sort(c(unique(police.data$city), "All"))
 
 GetTimeFilterCity <- function(city.crime){
   result <- filter(police.data, city == city.crime) %>% 
