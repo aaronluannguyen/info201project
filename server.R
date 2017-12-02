eef52af59a198925794416feb9b93fdc3978f379
Aaron Nguyen <aaronluannguyen@gmail.com>
2017-12-01 18:29:07 -0800

    Cleaned data for typos in city column

--- server.R
+++ server.R
@@ -15,7 +15,20 @@ library(plotly)
 
 # Load king county police data 
 # Remember to set working directory in console to this project
-data <- read.csv("data/King_County_Police_Data.csv")
+
+all.cities <- c("Auburn", "Barring", "Beaux Arts", "Bellevue", "Black Diamond", "Bothell", "Burien", "Carnation", "Chelan", "Covington", 
+            "Des Moines", "Duvall", "Enumclaw", "Everett", "Fairwood", "Fall City", "Federal Way", "Gold Bar", "Grotto", "Hobart", 
+            "Issaquah", "Kenmore", "Kent", "Kirkland", "Lake Forest Park", "LynnWood", "Maple Valley", "Monroe", "Newcastle", "North Bend", 
+            "Preston", "Puyallup", "Ravensdale", "Redmond", "Renton", "Seatac", "Seattle", "Shoreline", "Skykomish", "Skyway", "Snoqualmie", 
+            "Sumner", "Tacoma", "Tigard", "Tukwila", "Vashon", "White Center", "Woodinville")
+
+# Read in data and clean it for any typos for city column
+data <- read.csv("data/King_County_Police_Data.csv") %>%
+  filter(grepl("Auburn|Barring|Beaux Arts|Bellevue|Black Diamond|Bothell|Burien|Carnation|Chelan|Covington| 
+                 Des Moines|Duvall|Enumclaw|Everett|Fairwood|Fall City|Federal Way|Gold Bar|Grotto|Hobart| 
+                 Issaquah|Kenmore|Kent|Kirkland|Lake Forest Park|LynnWood|Maple Valley|Monroe|Newcastle|North Bend| 
+                 Preston|Puyallup|Ravensdale|Redmond|Renton|Seatac|Seattle|Shoreline|Skykomish|Skyway|Snoqualmie| 
+                 Sumner|Tacoma|Tigard|Tukwila|Vashon|White Center|Woodinville", city, ignore.case = TRUE))
 
 crime.types <- distinct(data, parent_incident_type)
 
