de2c1abfcfb1b1b66fc64cc2489e3bee19db9eb1
Aaron Nguyen <aaronluannguyen@gmail.com>
2017-12-03 04:05:29 -0800

    Finished including all information for pop up. Now working on other visuals to put on control panel

--- scripts/dashboard-aaron.R
+++ scripts/dashboard-aaron.R
@@ -21,12 +21,13 @@ Map <- function(data.df) {
     addCircleMarkers(data = data.df, 
                      lng = ~data.df$longitude, lat = ~data.df$latitude,
                      color = ~pal(data.df$parent_incident_type), fillOpacity = 1,
-                     popup = ~paste0(strong("Case Number"), " : ", data.df$case_number, "<br/>", 
-                                     strong("Incident Date & Time"), " : ", data.df$incident_datetime, "<br/>",
-                                     strong("Description"), " : ", data.df$incident_description, "<br/>",
-                                     strong("Address"), " : ", data.df$address_1, " ", data.df$city, ", ",
-                                     data.df$state, " ", data.df$zip
-                                       ), 
+                     popup = ~paste0(strong("Case Number: "), data.df$case_number, "<br>", 
+                                     strong("Incident Date & Time: "), data.df$incident_datetime, "<br>",
+                                     strong("Description: "), data.df$incident_description, "<br>",
+                                     strong("Address: "), data.df$address_1, " <br>", 
+                                     "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp",
+                                     data.df$city, ", ", data.df$state, " ", data.df$zip
+                                     ), 
                      clusterOptions = markerClusterOptions()) %>%
     addLegend("topright", title = "Crime Classification Colors", pal=pal, values = data.df$parent_incident_type, opacity = 1) %>%
     setView(lng = -120.74, lat = 45.7511, zoom = 7)
