c1a4ca117e760e8e6c268e0ad79192e68a944aa0
Chianson Siu <chanmancu@gmail.com>
2017-12-05 17:11:46 -0800

    Add stats file

---
+++ scripts/stats_chianson.R
@@ -0,0 +1,48 @@
+source("scripts/clean_data_chianson.R")
+library(dplyr)
+library(stringr)
+library(stats)
+
+# Read in csv file
+police.data <- CleanseData("data/King_County_Police_Data.csv")
+
+# Overall total number of calls from 2000 - 2017
+total.calls <- nrow(police.data)
+
+# Function to get frequency table of various columns in the king county data set
+# @param column.name is the case-sensitive name of the column being queried.
+# @returns a dataframe of the frequencies for desired column name
+GetFreqTable <- function(column.name){
+  result <- select(police.data, eval(parse(text = "column.name"))) %>% 
+            group_by_(eval(parse(text = "column.name"))) %>% 
+            summarise(Freq = n())
+}
+
+# Stats on property crime calls (most frequently called about)
+incident.types <- sort(unique(police.data$parent_incident_type))
+incident.freq.table <- GetFreqTable("parent_incident_type")
+property.crime.total <- filter(incident.freq.table, parent_incident_type == "Property Crime") %>% 
+                        pull(Freq)
+property.crime.percent <- round(property.crime.total / sum(incident.freq.table$Freq) * 10000) / 100
+
+# Stats on call days (average and standard deviation)
+day.of.call.table <- GetFreqTable("day_of_week")
+day.of.call.avg <- mean(day.of.call.table$Freq)
+day.of.call.std <- sd(day.of.call.table$Freq)
+
+# Stats on city calls
+city.freq.table <- GetFreqTable("city")
+city.most.calls <- pull(filter(city.freq.table, Freq == max(Freq)), city)
+city.most.calls.total <- pull(filter(city.freq.table, Freq == max(Freq)), Freq)
+city.least.calls <- pull(filter(city.freq.table, Freq == min(Freq)), city)
+city.least.calls.total <- pull(filter(city.freq.table, Freq == min(Freq)), Freq)[1]
+
+# Stats on call times
+time.freq.table <- GetFreqTable("hour_of_day")
+time.most.calls <- pull(filter(time.freq.table, Freq == max(Freq)), hour_of_day)
+time.most.calls.total <- pull(filter(time.freq.table, Freq == max(Freq)), Freq)
+time.least.calls <- pull(filter(time.freq.table, Freq == min(Freq)), hour_of_day)
+time.least.calls.total <- pull(filter(time.freq.table, Freq == min(Freq)), Freq)[1]
+
+
+
---
+++ scripts/time_filter_chianson.R
@@ -0,0 +1,32 @@
+source("clean_data_script.R")
+library(dplyr)
+library(plotly)
+library(stringr)
+
+police.call.data <- select(CleanseData("../../data/King_County_Police_Data.csv"),
+                           city, hour_of_day)
+
+# Creates and returns an interactive bargraph based on the city passed in.
+# @param city.crime is the case-insensitive name of the city the graph is being drawn for
+# @return is plotly bar graph object
+GetTimeFilterCity <- function(city.crime){
+    city.crime <- str_to_title(city.crime) # standardize case of city name passed in
+    result.data <- filter(police.call.data, city == city.crime) %>%
+                   table()
+    result.data <- as.data.frame(result.data)
+    # Create bar graph of results
+    result.graph <- plot_ly(
+        x = result.data$hour_of_day,
+        y = result.data$Freq,
+        type = "bar",
+        text = paste0("Hour: ", result.data$hour_of_day, "\n", "Calls: ", result.data$Freq),
+        hoverinfo = "text"
+    ) %>%
+        layout(
+            yaxis = list(title = "Number of Calls"),
+            xaxis = list(title = str_to_title(paste("Time of Calls in", city.crime, "(24hr clock)", sep = " ")),
+                         tickangle = 0),
+            title = str_to_title(paste("Police Call Frequency in", city.crime, "(2017)", sep = " "))
+    )
+    return(result.graph)
+}
