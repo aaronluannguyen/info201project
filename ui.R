7f78b3b24943fb59754e0007c3151aabba6ae6a5
Aaron Nguyen <aaronluannguyen@gmail.com>
2017-12-06 19:27:52 -0800

    Renamed rmd file to be specific towards our team tab

--- ui.R
+++ ui.R
@@ -110,23 +110,8 @@ shinyUI(fluidPage(
                         )
                       ),
              
-             tabPanel("Our Team", includeMarkdown("RMarkdownFile.rmd"),
-                      h1("Team Members"),
-                      br(),
-                      
-                      h3("Aaron Nguyen"),
-                      p("Primary role was creating the strong(em(Crime Map))."),
-                      br(),
-                      
-                      h3("Chianson Siu"),
-                      
-                      br(),
-                      
-                      h3("Keivon Gunn"),
-                      
-                      br(),
-                      
-                      h3("Omid Vahabnejad")
+             tabPanel("Our Team", 
+                      includeMarkdown("RMarkdownFile.rmd"),
                       
                     )
              
